3a3a715a15a04f10306cff493148d2959b747063
Songbeom00 <kjb001023@naver.com>
2024-12-16 13:30:08 +0900

    main

--- router/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const usersController = require('../controller/users');
-
-router.get('/', usersController.getAllUsers);
-// router.get('/:id', usersController.getUserById);
-
-module.exports = router;
