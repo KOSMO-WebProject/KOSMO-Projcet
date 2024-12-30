d3faf25140ed1f7775c40abaf0f57e5d50f0d5fe
mm0307y <mm0307a@gmail.com>
2024-12-30 18:38:19 +0900

    로그인 및 회원가입

--- server/controller/auth.js
+++ server/controller/auth.js
@@ -7,7 +7,7 @@ const register = async (req, res) => {
   try {
     const [rows] = await db
       .get()
-      .execute("SELECT * FROM user WHERE id = ?", [req.body.user_id]);
+      .execute("SELECT * FROM user WHERE user_id = ?", [req.body.user_id]);
     if (rows.length > 0) {
       return res.status(409).json("해당 유저가 이미 존재합니다.");
     }
@@ -15,33 +15,29 @@ const register = async (req, res) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(req.body.user_pw, salt);
 
-    const q =
-      "INSERT INTO user(`user_id`,`user_pw`,`user_name`,`phone_number`,`email`,`date_of_birth`,`gender`) VALUES (?)";
-    const values = [
+    const q = "INSERT INTO user(user_id, user_pw, user_name) VALUES (?)";
+    values = [
       req.body.user_id,
       hashedPassword,
       req.body.user_name,
-      req.body.phone_number,
-      req.body.email,
-      req.body.date_of_birth,
-      req.body.gender,
-    ];
-
-    const q1 =
-      "insert into address(`user_id`, `recipient_name`, `phone_number`, `postal_code`, `address`, `detailed_address`, `is_default`) values(?)";
-    const values1 = [
-      req.body.user_id,
-      req.body.recipient_name,
       req.body.phone_number,
       req.body.postal_code,
       req.body.address,
       req.body.detailed_address,
       req.body.is_default,
     ];
+    const [rows1] = await db.get().execute(q, values);
+    console.log(rows1);
 
-    await db.get().execute(q, [values]);
-    await db.get().execute(q1, [values1]);
-    return res.status(200).json("회원가입이 완료되었습니다.");
+    if (rows1) {
+      const q = "SELECT * FROM user WHERE user_id = ?";
+      const [rows2] = await db.get().execute(q, [req.body.user_id]);
+      const id = rows2[0].id;
+
+      const q1 = "INSERT INTO address (user_id, recipient_name) VALUES (?, ?)";
+      await db.get().execute(q1, [id, req.body.user_name]);
+      return res.status(201).json("회원가입이 완료되었습니다.");
+    }
   } catch (error) {
     console.error(error);
     return res.status(500).json(error);
@@ -53,31 +49,31 @@ const login = async (req, res) => {
   try {
     const [rows] = await db
       .get()
-      .execute("SELECT * FROM user WHERE user_name = ?", [req.body.username]);
+      .execute("SELECT * FROM user WHERE user_id = ?", [req.body.user_id]);
     if (rows.length === 0) {
       return res.status(400).json("아이디 또는 비밀번호가 틀렸습니다.");
     }
 
     const checkPassword = await bcrypt.compare(
-      req.body.password,
-      rows[0].password
+      req.body.user_pw,
+      rows[0].user_pw
     );
     if (!checkPassword) {
       return res.status(400).json("아이디 또는 비밀번호가 틀렸습니다!");
     }
 
     const accessToken = jwt.sign(
-      { user_id: rows[0].user_id },
+      { user_id: rows[0].id },
       process.env.ACCESS_SECRET,
       { expiresIn: "1h" }
     );
     const refreshToken = jwt.sign(
-      { user_id: rows[0].user_id },
+      { user_id: rows[0].id },
       process.env.REFRESH_SECRET,
       { expiresIn: "24h" }
     );
 
-    const { password, ...others } = rows[0];
+    const { user_pw, ...others } = rows[0];
 
     res
       .cookie("accessToken", accessToken, { httpOnly: true })
--- server/router/auth.js
+++ server/router/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require('../controller/auth');
 
 router.post('/register', authController.register)
-      .post("/login",authController.login) //클라이언트가 /auth/login으로 post 요청을 보내면
+router.post("/login",authController.login) //클라이언트가 /auth/login으로 post 요청을 보내면
 router.post("/logout",authController.logout)
 router.get("/accesstoken",authController.accessToken)
 
