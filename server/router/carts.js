3fea6b7f3ec6c6f8c191cc8e8a19742e1d4e60fc
alstjr2171@gmail.com <alstjr2171@gmail.com>
2025-01-20 18:12:32 +0900

    QNA 추가

--- server/router/carts.js
+++ server/router/carts.js
@@ -3,19 +3,11 @@ const router = express.Router();
 const cartsController = require("../controller/carts");
 
 //carts
-<<<<<<< HEAD
 router
   .get("/item/:user_no", cartsController.cartList)
   .post("/item", cartsController.cartInsert)
+  // .put("/item/:cart_item_no",cartsController.cartUpdate)
   .delete("/item/:cart_item_no", cartsController.cartOneDelete)
   .delete("/:cart_no", cartsController.cartAllDelete);
-//router.put("/item/:cart_item_no",cartsController.cartUpdate)
-=======
-router.get("/item/:user_no",cartsController.cartList)
-      .post("/item",cartsController.cartInsert)
-      // .put("/item/:cart_item_no",cartsController.cartUpdate)
-      .delete("/item/:cart_item_no",cartsController.cartOneDelete)
-      .delete("/:cart_no",cartsController.cartAllDelete)
->>>>>>> jongbeom
 
 module.exports = router;
